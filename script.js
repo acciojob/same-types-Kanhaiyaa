25673cafe585da08f31815c4b0394c1017a48633
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-11 10:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function isSameType(value1, value2) {
   //your js code here
+	if (isSameType>=48 && isSameType<=57) {
+		console.log("true")
+	}else if(isSameType=="hey" && isSameType=="hello"){
+		console.log("true")
+	}else if (isSameType !>=48 && isSameType!<=57) {
+		console.log("true")
+	}else if (isSameType =="3" && isSameType==3) {
+		console.log("false")
+	}
+		
+	
 }
 
 // do not change the code below.
